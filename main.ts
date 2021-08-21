50f38fcffea8824a0e6092b3c0b6f1e498e8a1d4
shivamjoker <singhshivamkr@gmail.com>
2021-08-21 12:21:14 +0530

    feat: initial beta app

--- main.ts
+++ main.ts
@@ -3,6 +3,14 @@ import * as Tone from "tone";
 import { Frequency, Synth } from "tone";
 import { convertMelody, saveFile } from "./arduino";
 
+const [playSymbol, stopSymbol] = [
+  `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
+    `,
+  `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M6 6h12v12H6z"></path></svg>
+  `,
+];
 const trackSelector = document.getElementById("tracks") as HTMLSelectElement;
 const playPauseBtn = document.getElementById(
   "play-toggle"
@@ -11,14 +19,18 @@ const codeTextArea = document.getElementById(
   "arduinoCode"
 ) as HTMLTextAreaElement;
 const downloadBtn = document.getElementById("downloadBtn");
+const errorTxt = document.getElementById("error");
 
+const renderError = (msg: string) => {
+  errorTxt.innerText = msg;
+};
 if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
   document.querySelector("#FileDrop #Text").textContent =
     "Reading files not supported by this browser";
 } else {
   const fileDrop = document.querySelector("#FileDrop");
 
-  fileDrop.addEventListener("dragenter", () => fileDrop.classList.add("Hover"));
+  fileDrop.addEventListener("dragover", () => fileDrop.classList.add("Hover"));
 
   fileDrop.addEventListener("dragleave", () =>
     fileDrop.classList.remove("Hover")
@@ -71,13 +83,17 @@ function parseFile(file: File) {
   const reader = new FileReader();
   reader.onload = function (e) {
     //@ts-ignore
-    const midi = new Midi(e.target.result);
-    console.log(file);
-    playPauseBtn.removeAttribute("disabled");
-    currentMidi = midi;
-    renderTracks();
-    generateCode();
-    fileName = file.name.split(".")[0] + ".ino";
+    try {
+      const midi = new Midi(e.target.result);
+      playPauseBtn.removeAttribute("disabled");
+      currentMidi = midi;
+      renderTracks();
+      generateCode();
+      fileName = file.name.split(".")[0] + ".ino";
+      errorTxt.innerText = "";
+    } catch (err) {
+      renderError("Error in parsing " + err);
+    }
   };
   reader.readAsArrayBuffer(file);
 }
@@ -86,22 +102,27 @@ const synths: Synth[] = [];
 
 playPauseBtn.addEventListener("click", () => {
   if (currentMidi && !synths.length) {
-    playPauseBtn.innerText = "Stop";
+    playPauseBtn.innerHTML = stopSymbol;
     const now = Tone.now();
     //create a synth for each track
     const track = currentMidi.tracks[+trackSelector.value];
     const synth = new Tone.Synth().toDestination();
     synths.push(synth);
-    synth.onsilence = () => {
-      playPauseBtn.innerText = "Play";
-    };
+
+    // synth.onsilence = () => {
+    //   playPauseBtn.innerText = playSymbol;
+    // };
     track.notes.forEach((note) => {
-      synth.triggerAttackRelease(note.name, note.duration, note.time + now);
+      try {
+        synth.triggerAttackRelease(note.name, note.duration, note.time + now);
+      } catch (err) {
+        console.error(err);
+      }
     });
   } else {
     //dispose the synth and make a new one
     const synth = synths.shift();
     synth.dispose();
-    playPauseBtn.innerText = "Play";
+    playPauseBtn.innerHTML = playSymbol;
   }
 });
